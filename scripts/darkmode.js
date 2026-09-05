907758c8b8d677c85b165777995ae4037c444f5e
Joseph <joelaw102@icloud.com>
2026-09-05 16:34:21 +0100

    initial website

---
+++ scripts/darkmode.js
@@ -0,0 +1,20 @@
+const toggleSwitch = document.getElementById('darkModeToggle');
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+toggleSwitch.addEventListener('change', function (e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+});
